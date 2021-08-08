88c45372d158795b4651263c660f47d41b6c0bbc
Alexandra Gomez <iagomez@bancolombia.com.co>
2021-08-08 16:06:01 -0500

    configuration routing

--- movies/src/app/app-routing.module.ts
+++ movies/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShowDetailsMovieComponent } from './components/show-details-movie/show-details-movie.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'show-details-movie', component: ShowDetailsMovieComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
